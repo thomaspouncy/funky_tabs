af2f2a8599bf4d835b21455a97ef07405d6c515f
thomaspouncy <thomas.pouncy@gmail.com>
2011-05-16 16:01:35 -0700

    pass parameters to render method

--- lib/funky_tabs/renderers.rb
+++ lib/funky_tabs/renderers.rb
@@ -7,9 +7,9 @@ module FunkyTabs
 
     module InstanceMethods
       def render(*args, &block)
-        super and return if params[:funky_tabs].nil?
+        super(*args, &block) and return if params[:funky_tabs].nil?
         @tab_index = FunkyTabs.tab_index_from_location_hash(params[:location_hash])
-        super and return if @tab_index.nil?
+        super(*args, &block) and return if @tab_index.nil?
         @location_hash = params[:location_hash]
         @render_content = render_to_string(*args, &block)
         super(:template=>"funky_tabs/update/update_tab",:layout=>false)
