8edeb21ad6a528199f5bbe22f4ef21eeb64667e9
thomaspouncy <thomas.pouncy@gmail.com>
2011-05-21 18:23:53 -0700

    added documentation and fixed some minor bugs

--- lib/funky_tabs/renderers.rb
+++ lib/funky_tabs/renderers.rb
@@ -8,8 +8,6 @@ module FunkyTabs
     module InstanceMethods
       def render(*args, &block)
         super(*args, &block) and return if params[:funky_tabs].nil?
-        @tab_index = FunkyTabs.tab_index_from_location_hash(params[:location_hash])
-        super(*args, &block) and return if @tab_index.nil?
         logger.debug "full path is #{request.fullpath}"
         if FunkyTabs.correct_path_for_location_hash?(request.fullpath,params[:location_hash])
           @location_hash = params[:location_hash]
@@ -17,6 +15,8 @@ module FunkyTabs
           @location_hash = FunkyTabs.location_hash_for_content_path(request.fullpath)
         end
         super(*args, &block) and return if @location_hash.nil?
+        @tab_index = FunkyTabs.tab_index_from_location_hash(@location_hash)
+        super(*args, &block) and return if @tab_index.nil?
         @render_content = render_to_string(*args, &block)
         super(:template=>"funky_tabs/update/update_tab",:layout=>false)
       end
