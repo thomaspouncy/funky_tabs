29254e8330d1e654f8142b6771ed6e87f9d60f0a
thomaspouncy <thomas.pouncy@gmail.com>
2011-06-01 11:18:30 -0700

    fixed bug that caused render and return commands to throw Double Render exception

--- lib/funky_tabs/renderers.rb
+++ lib/funky_tabs/renderers.rb
@@ -7,13 +7,13 @@ module FunkyTabs
 
     module InstanceMethods
       def render(*args, &block)
-        super(*args, &block) and return if params[:funky_tabs].nil?
+        return super(*args, &block) if params[:funky_tabs].nil?
         @location_hash = params[:location_hash]
-        super(*args, &block) and return if @location_hash.nil?
+        return super(*args, &block) if @location_hash.nil?
         @tab_index = FunkyTabs.tab_index_from_location_hash(@location_hash)
-        super(*args, &block) and return if @tab_index.nil?
+        return super(*args, &block) if @tab_index.nil?
         @render_content = render_to_string(*args, &block)
-        super(:template=>"funky_tabs/update/update_tab",:layout=>false)
+        return super(:template=>"funky_tabs/update/update_tab",:layout=>false)
       end
     end
   end
